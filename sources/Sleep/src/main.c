c25daf8947cc2817b192fe78de9f2c56b834002c
Sasha7b9Work <Phuntik-it@tut.by>
2025-05-16 12:26:38 +0300

    Проект Keil компилируется с обновлённым CubeF3

--- sources/Sleep/src/main.c
+++ sources/Sleep/src/main.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+
+#ifndef WIN32
+    #if __ARMCC_VERSION != 6210000
+        // На других версиях компиляторов не проверялось
+        // Но на 6.23 (и, видимо, 6.22) из Keil 5.42a вылетает с BKPT 0xAB. Нужна реализация putc, etc for _sys_open (файл Retarget.c)
+        #error "Requires ARM Compiler V6.21 from uVision 5.39"
+    #endif
+#endif
+
 /** @addtogroup STM32F3xx_HAL_Examples
   * @{
   */
